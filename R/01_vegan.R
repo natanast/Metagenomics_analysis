b725d9456961b593f2df99aaa603ff4b0cef884c
Natasa Anastasiadou <141711668+natanast@users.noreply.github.com>
2025-11-24 13:40:27 +0200

    vegan
    Load data from 'amr-counts.txt' and prepare counts matrix for analysis.

--- R/01_vegan.R
+++ R/01_vegan.R
@@ -7,3 +7,22 @@ gc()
 
 library(data.table)
 library(vegan)
+
+# load data ---------
+
+d = "amr-counts.txt" |> fread()
+
+
+d = d[, c(1, 7:ncol(d)), with = FALSE]
+
+
+# rarefaction curves -----
+# keep only numeric counts (remove Geneid column)
+counts_matrix <- d[, -1, with = FALSE]  # all columns except first
+
+# convert to matrix
+counts_matrix <- as.matrix(counts_matrix)
+
+# transpose so rows = samples, columns = genes
+counts_matrix_t <- t(counts_matrix)
+
