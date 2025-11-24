1e8e410aa2561cb7d93332741806c6e3d40418d6
Natasa Anastasiadou <141711668+natanast@users.noreply.github.com>
2025-11-24 13:41:18 +0200

    vegan

--- R/01_vegan.R
+++ R/01_vegan.R
@@ -12,11 +12,10 @@ library(vegan)
 
 d = "amr-counts.txt" |> fread()
 
+# data cleaning -------
 
 d = d[, c(1, 7:ncol(d)), with = FALSE]
 
-
-# rarefaction curves -----
 # keep only numeric counts (remove Geneid column)
 counts_matrix <- d[, -1, with = FALSE]  # all columns except first
 
@@ -26,3 +25,12 @@ counts_matrix <- as.matrix(counts_matrix)
 # transpose so rows = samples, columns = genes
 counts_matrix_t <- t(counts_matrix)
 
+
+# rarefaction curves -----
+rarecurve(
+    counts_matrix_t, 
+    step = 100,          # increment of sample size
+    sample = min(rowSums(counts_matrix_t)), # rarefy to the smallest sample depth
+    col = "darkgreen",
+    label = FALSE
+)
