9b73a3ccff687fbd98570ec6d71d37518a6b1c91
Natasa Anastasiadou <141711668+natanast@users.noreply.github.com>
2025-11-24 14:41:12 +0200

    Update 01_vegan.R

--- R/01_vegan.R
+++ R/01_vegan.R
@@ -6,15 +6,23 @@ gc()
 # libraries ---------
 
 library(data.table)
+library(stringr)
 library(vegan)
 
 # load data ---------
 
 d = "amr-counts.txt" |> fread()
 
-# data cleaning -------
 
 d = d[, c(1, 7:ncol(d)), with = FALSE]
+
+
+colnames(d) = d |>
+    colnames() |>
+    str_remove_all("bwa_amr_output|sorted|bam|\\.|\\/|_L001_amr|")
+
+
+
 
 # keep only numeric counts (remove Geneid column)
 counts_matrix <- d[, -1, with = FALSE]  # all columns except first
@@ -32,5 +40,29 @@ rarecurve(
     step = 100,          # increment of sample size
     sample = min(rowSums(counts_matrix_t)), # rarefy to the smallest sample depth
     col = "darkgreen",
-    label = FALSE
+    label = TRUE
+    # xlab = "Sequencing depth (reads)", 
+    # ylab = "Observed genes"
 )
+
+
+
+# 
+# library(ggplot2)
+# # keep only numeric counts
+# counts_matrix <- as.matrix(d[, -1, with = FALSE])
+# 
+# # calculate total abundance per sample
+# total_abundance <- colSums(counts_matrix)
+# 
+# # create a simple plot
+# df <- data.frame(
+#     Sample = colnames(counts_matrix),
+#     TotalCounts = total_abundance
+# )
+# 
+# ggplot(df, aes(x = Sample, y = TotalCounts)) +
+#     geom_bar(stat = "identity") +
+#     theme_bw() +
+#     ylab("Total abundance") +
+#     xlab("Sample")
