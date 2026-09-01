6433edc54669a49700b3936e0cf41acb9a84ddca
NatAnastas <anastasiadounat@gmail.com>
2026-09-01 10:19:32 +0300

    diversity script

--- R/08_diversity.R
+++ R/08_diversity.R
@@ -12,3 +12,14 @@ library(vegan)
 
 
 # load data ---------
+
+library(data.table)
+library(stringr)
+library(vegan)
+
+
+# load data ------
+
+d1 = "abundance_table_species.tsv" |> fread()
+
+d2 = "sample_metadata.csv" |> fread()
