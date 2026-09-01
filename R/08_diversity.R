e42d9184fd37485088e40aaa6e7503985808c05a
NatAnastas <anastasiadounat@gmail.com>
2026-09-01 10:19:46 +0300

    diversity script

--- R/08_diversity.R
+++ R/08_diversity.R
@@ -23,3 +23,19 @@ library(vegan)
 d1 = "abundance_table_species.tsv" |> fread()
 
 d2 = "sample_metadata.csv" |> fread()
+
+# clean data -----
+
+d1 <-  d1[, c(1, 4:ncol(d1)), with = FALSE]
+
+
+index <-  colnames(d1) |> str_detect("\\.bracken_num")
+d <-  d1[, index, with = FALSE]
+
+
+mat <- as.matrix(d)
+colnames(d) <- colnames(d) |> str_split_i("\\_L001", 1)
+rownames(mat) <- d1$name
+
+
+mat <- t(d)
